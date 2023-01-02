7327b3d9451b4583e7ce33054ebceecc7d5aa8d7
Jose Francisco <jfteixeira@yahoo.com.br>
2023-01-01 22:45:34 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc_.Net7/Data/SalesWebMvc_Net7Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc_.Net7.Models;
+
+namespace SalesWebMvc_.Net7.Data
+{
+    public class SalesWebMvc_Net7Context : DbContext
+    {
+        public SalesWebMvc_Net7Context (DbContextOptions<SalesWebMvc_Net7Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc_.Net7.Models.Department> Department { get; set; } = default!;
+    }
+}
--- SalesWebMvc_.Net7/Program.cs
+++ SalesWebMvc_.Net7/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc_.Net7.Data;
 namespace SalesWebMvc_.Net7
 {
     public class Program
@@ -5,6 +8,8 @@ namespace SalesWebMvc_.Net7
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<SalesWebMvc_Net7Context>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvc_Net7Context") ?? throw new InvalidOperationException("Connection string 'SalesWebMvc_Net7Context' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
