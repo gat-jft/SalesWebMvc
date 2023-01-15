1276799054c2e0c21dd7f0cae2adb2fae0d36000
Jose Francisco <jfteixeira@yahoo.com.br>
2023-01-15 04:09:28 -0300

    SellersController

---
+++ SalesWebMvc_.Net7/Controllers/SellersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc_.Net7.Models;
+
+namespace SalesWebMvc_.Net7.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {          
+            return View();
+        }
+    }
+}
