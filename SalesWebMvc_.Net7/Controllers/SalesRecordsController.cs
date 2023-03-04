b5adbfa8dffe9d4e923af0ad1209b2ba097455d7
Jose Francisco <jfteixeira@yahoo.com.br>
2023-03-04 19:43:03 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc_.Net7/Controllers/SalesRecordsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc_.Net7.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // SimpleSearch = Busca simples.
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        // SimpleSearch = Busca simples.
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
